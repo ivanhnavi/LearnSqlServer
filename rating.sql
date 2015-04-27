704cff2dbac010100e13e67b99eb8d614f47ca76
ivanhnavi <hernandez.ivan.u@gmail.com>
2015-04-27 10:12:30 -0700

    Update rating.sql
    Added Movie-Rating Query Extra Exercises

--- rating.sql
+++ rating.sql
@@ -123,3 +123,60 @@ GROUP by title) AS B,
 FROM Movie M JOIN Rating Ra ON M.mID = Ra.mID
 WHERE year > 1980
 GROUP by title) AS A
+
+
+---------------Added the Movie-Rating Query Extra Exercises----------------
+
+--Question 1 Find the names of all reviewers who rated Gone with the Wind
+SELECT DISTINCT name FROM 
+Movie M JOIN Rating Ra on M.mID = Ra.mID
+JOIN Reviewer R on Ra.rID = R.rID
+WHERE title = 'Gone with the Wind'
+
+--Quetsion 2 For any rating where the reviewer is the same as the director of the movie,
+--return the reviewer name, movie title, and number of stars.
+SELECT name, title, stars FROM 
+Movie M JOIN Rating Ra on M.mID = Ra.mID
+JOIN Reviewer R on Ra.rID = R.rID
+WHERE name = director 
+
+--Question 3 Return all reviewer names and movie names together in a single list, alphabetized.
+--Sorting by the first name of the reviewer and first word in the title is fine;
+--no need for special processing on last names or removing "The".) 
+SELECT name FROM Reviewer
+UNION
+SELECT title FROM Movie
+Order by name, title
+
+--Question 4 Find the titles of all movies not reviewed by Chris Jackson. 
+SELECT title FROM Movie
+WHERE mID NOT IN (SELECT mID
+FROM Rating
+WHERE rID IN 
+(select rID
+FROM Reviewer
+WHERE name = "Chris Jackson") )
+
+--Question 5 For all pairs of reviewers such that both reviewers gave a rating to the same movie,
+--return the names of both reviewers. Eliminate duplicates, don't pair reviewers with themselves,
+--and include each pair only once. For each pair, return the names in the pair in alphabetical order. 
+
+--Question 6 For each rating that is the lowest (fewest stars) currently in the database, return the reviewer name,
+--movie title, and number of stars. 
+
+
+--Quesetion 7 List movie titles and average ratings, from highest-rated to lowest-rated. 
+--If two or more movies have the same average rating, list them in alphabetical order
+
+--Question 8 Find the names of all reviewers who have contributed three or more ratings.
+--(As an extra challenge, try writing the query without HAVING or without COUNT.) 
+
+--Question 9 Some directors directed more than one movie. For all such directors, return the titles of all movies directed
+--by them, along with the director name. Sort by director name, then movie title.
+--(As an extra challenge, try writing the query both with and without COUNT.) 
+
+--Question 10
+
+--Question 11
+
+--Question 12
